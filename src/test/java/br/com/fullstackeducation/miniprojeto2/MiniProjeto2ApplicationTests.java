9ffbdcfe3df8791c017923767a7040c76652c2ab
danielSteinheisen <daniel.steinheisen@yahoo.com>
2024-04-02 19:51:34 -0300

    principal

---
+++ src/test/java/br/com/fullstackeducation/miniprojeto2/MiniProjeto2ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fullstackeducation.miniprojeto2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniProjeto2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

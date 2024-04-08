506a06069a59a909e0790533711fa4533d525477
ajjaum <j061701@dac.unicamp.br>
2024-04-07 22:01:56 -0300

    Ex 02, falta Log.

--- src/main/java/br/com/fullstackeducation/miniprojeto2/service/AlunoService.java
+++ src/main/java/br/com/fullstackeducation/miniprojeto2/service/AlunoService.java
@@ -1,9 +1,23 @@
 package br.com.fullstackeducation.miniprojeto2.service;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
+import br.com.fullstackeducation.miniprojeto2.dto.AlunoFiltro;
+import br.com.fullstackeducation.miniprojeto2.entity.AlunoEntity;
 
-@Service
-@RequiredArgsConstructor
-public class AlunoService {
+import java.util.List;
+
+public interface AlunoService {
+
+    //CRUD
+
+    AlunoEntity criar(AlunoEntity aluno) throws Exception; //Create
+
+    List<AlunoEntity> buscarTodos(AlunoFiltro filtro); //Read
+
+    AlunoEntity buscarPorId(Long id) throws Exception; //Read
+
+    AlunoEntity buscarPorNome(String nome) throws Exception;
+
+    AlunoEntity alterar(Long id, AlunoEntity aluno) throws Exception; //Update
+
+    void apagar(Long id) throws Exception; //Delete
 }
